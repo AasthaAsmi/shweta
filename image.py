68c6c5677502b13da5d78e7bfe0f0e4487fe1dca
AasthaAsmi <32697131+AasthaAsmi@users.noreply.github.com>
2017-10-11 15:57:08 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,3 +5,7 @@ from math import sqrt
 from skimage.color import rgb2gray
 import glob
 from skimage.io import imread
+example_file = glob.glob(r"C:UsersTavishDesktopwint_sky.gif")[0]
+im = imread(example_file, as_grey=True)
+plt.imshow(im, cmap=cm.gray)
+plt.show()
